65d8011632186aaeb3bcb6b6a73ef720a9d5d8d2
KenpachiZ11 <mr.daniil.b@mail.ru>
2021-04-13 05:22:56 +0300

    homework 4

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const player1 = {
     img: 'https://www.fightersgeneration.com/nf8/char6/scorpion-umk3-spear-toss-fan-remake.GIF',
     weapon: ['Kunai'],
     attack: () => console.log(this.name + ' ' + 'Fight...'),
+    changeHP: changeHP,
+    elHP:  elHP,
+    renderHP: renderHP,
 }
 
 const player2 = {
@@ -17,6 +20,9 @@ const player2 = {
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['Blade'],
     attack: () => console.log(this.name + ' ' + 'Fight...'),
+    changeHP: changeHP,
+    elHP:  elHP,
+    renderHP: renderHP,
 }
 
 function createElement (tag, className) {
@@ -47,43 +53,73 @@ function createPlayer(pers) {
     return $player;
 }
 
-function changeHP(player) {
-    const $playerLife = document.querySelector('.player' + player.player + ' ' +'.life');
-    $playerLife.style.width = player.hp + '%';
-    
-    console.log(player.hp)
-
-    player.hp -= Math.ceil(Math.random() * 20);
-
-    if (player.hp <= 0 && player.player === 1) {
-        player.hp = 0;
-        $arenas.appendChild(playerStatus(player2.name));
-        console.log('Решающим ударом отправляем в = ' + `${player.hp}`);
-      }
-    
-      if (player.hp <= 0 && player.player === 2) {
-        player.hp = 0;
-        $arenas.appendChild(playerStatus(player1.name));
-        console.log('Решающим ударом отправляем в = ' +`${player.hp}`);
-      }     
+function getRandom(num) {
+    return Math.ceil(Math.random() * num);
 }
 
-$clickButton.addEventListener('click', () => {
-    // console.log('тык!');
-    changeHP(player1)
-        console.log(player1.name);
-    changeHP(player2);
-        console.log(player2.name);
+function changeHP() {
+    this.hp -= getRandom(20);
+    if(this.hp <= 0) {
+        this.hp = 0;
+    }
+}
+
+function elHP() {
+    const $playerLife =  document.querySelector('.player' + this.player + ' ' +'.life');
+    return $playerLife;
+}
+
+function renderHP() {
+    this.elHP().style.width = this.hp + '%';
+}
+
+$clickButton.addEventListener('click', function () {
+    player1.changeHP(getRandom());
+    player1.renderHP();
+    console.log(player1.hp);
+
+    player2.changeHP(getRandom());
+    player2.renderHP();
+    console.log(player2.hp);
+
+        if(player1.hp === 0 || player2.hp === 0) {
+            $clickButton.disabled = true;
+            createReloadButton();
+        }
+
+        if(player1.hp === 0 && player1.hp < player2.hp) {
+            $arenas.appendChild(playerStatus(player2.name));
+        } else if((player2.hp === 0 && player2.hp < player1.hp)) {
+            $arenas.appendChild(playerStatus(player1.name));
+        } else if((player1.hp === 0 && player2.hp === 0)) {
+            $arenas.appendChild(playerStatus());
+        }
 })
+
+function createReloadButton() {
+    const $reloadWrap = createElement('div', 'reloadWrap');
+    const $reloadButton = createElement('button', 'button');
+
+    $reloadButton.innerText = 'Reload';
+    $reloadWrap.appendChild($reloadButton);
+    $arenas.appendChild($reloadWrap);
+
+    $reloadButton.addEventListener('click', () => {
+        window.location.reload();
+    });
+}
 
 function playerStatus(name) {
     const $closeTitle = createElement('div', 'winTitle');
-    $closeTitle.innerText = name + ' ' + 'win';
+    if(name){
+        $closeTitle.innerText = name + ' ' + 'win';
+    } else {
+        $closeTitle.innerText = 'draw';
+    }
     $clickButton.disabled = true;
     return $closeTitle;
 }
 
 $arenas.appendChild(createPlayer(player1));    
 $arenas.appendChild(createPlayer(player2));    
-
 
