9ea4db9ad88f41d5eb52a0b197423b94a0f85877
KenpachiZ11 <mr.daniil.b@mail.ru>
2021-04-16 16:25:35 +0300

    Homework - 5

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const $arenas = document.querySelector('.arenas');
 const $clickButton = document.querySelector('.button');
+const $formFight = document.querySelector('.control');
+
 
 const player1 = {
     player: 1,
@@ -8,9 +10,9 @@ const player1 = {
     img: 'https://www.fightersgeneration.com/nf8/char6/scorpion-umk3-spear-toss-fan-remake.GIF',
     weapon: ['Kunai'],
     attack: () => console.log(this.name + ' ' + 'Fight...'),
-    changeHP: changeHP,
-    elHP:  elHP,
-    renderHP: renderHP,
+    changeHP,
+    elHP,
+    renderHP,
 }
 
 const player2 = {
@@ -20,10 +22,18 @@ const player2 = {
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['Blade'],
     attack: () => console.log(this.name + ' ' + 'Fight...'),
-    changeHP: changeHP,
-    elHP:  elHP,
-    renderHP: renderHP,
+    changeHP,
+    elHP,
+    renderHP,
 }
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+
+const ATTACK = ['head', 'body', 'foot'];
 
 function createElement (tag, className) {
     const $tag = document.createElement(tag);
@@ -57,8 +67,8 @@ function getRandom(num) {
     return Math.ceil(Math.random() * num);
 }
 
-function changeHP() {
-    this.hp -= getRandom(20);
+function changeHP(randomNumber) {
+    this.hp -= getRandom(randomNumber);
     if(this.hp <= 0) {
         this.hp = 0;
     }
@@ -73,28 +83,15 @@ function renderHP() {
     this.elHP().style.width = this.hp + '%';
 }
 
-$clickButton.addEventListener('click', function () {
-    player1.changeHP(getRandom());
-    player1.renderHP();
-    console.log(player1.hp);
+// $clickButton.addEventListener('click', function () {
+//     player1.changeHP();
+//     player1.renderHP();
+//     console.log(player1.hp);
 
-    player2.changeHP(getRandom());
-    player2.renderHP();
-    console.log(player2.hp);
-
-        if(player1.hp === 0 || player2.hp === 0) {
-            $clickButton.disabled = true;
-            createReloadButton();
-        }
-
-        if(player1.hp === 0 && player1.hp < player2.hp) {
-            $arenas.appendChild(playerStatus(player2.name));
-        } else if((player2.hp === 0 && player2.hp < player1.hp)) {
-            $arenas.appendChild(playerStatus(player1.name));
-        } else if((player1.hp === 0 && player2.hp === 0)) {
-            $arenas.appendChild(playerStatus());
-        }
-})
+//     player2.changeHP();
+//     player2.renderHP();
+//     console.log(player2.hp);  
+// })
 
 function createReloadButton() {
     const $reloadWrap = createElement('div', 'reloadWrap');
@@ -110,7 +107,7 @@ function createReloadButton() {
 }
 
 function playerStatus(name) {
-    const $closeTitle = createElement('div', 'winTitle');
+    const $closeTitle = createElement('div', 'loseTitle');
     if(name){
         $closeTitle.innerText = name + ' ' + 'win';
     } else {
@@ -123,3 +120,60 @@ function playerStatus(name) {
 $arenas.appendChild(createPlayer(player1));    
 $arenas.appendChild(createPlayer(player2));    
 
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence,
+    }
+}
+
+function Winner(player1, player2) {
+        if(player1.hp === 0 || player2.hp === 0) {
+        $clickButton.disabled = true;
+        createReloadButton();
+        }
+
+        if(player1.hp === 0 && player1.hp < player2.hp) {
+            $arenas.appendChild(playerStatus(player2.name));
+        } else if((player2.hp === 0 && player2.hp < player1.hp)) {
+            $arenas.appendChild(playerStatus(player1.name));
+        } else if((player1.hp === 0 && player2.hp === 0)) {
+            $arenas.appendChild(playerStatus());
+        }
+}
+
+function impactCheck(player, attack, enemy) {
+    if (attack.hit !== enemy.defence) {
+        player.changeHP(attack.value);
+        player.renderHP(player.elHP())
+    }
+}
+
+
+$formFight.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const enemy = enemyAttack();
+    const attack = {};
+
+    for(let item of $formFight) {
+        if(item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+        if(item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+        // item.checked = false;
+    }
+    console.log('Scorpion', attack.value, attack );
+    console.log('Kitana', enemy.value, enemy);
+
+    impactCheck(player2, attack, enemy);
+    impactCheck(player1, enemy, attack);
+    Winner(player1, player2);
+})
